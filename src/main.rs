e45872a0e43d865ec4ed70eb2a197268ce411da8
nokaa <nokaa@cock.li>
2016-11-05 16:38:51 -0500

    Upgrade dependencies, implement clippy lints

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate env_logger;
 extern crate hayaku;
 extern crate toml;
 
-use hayaku::{forms, Http, Request, ResponseWriter, Path, Status};
+use hayaku::{Http, Request, ResponseWriter, Path, Status};
 
 use std::fs;
 use std::io::Read;
@@ -33,46 +33,22 @@ fn main() {
 }
 
 fn search_handler(req: &Request, res: &mut ResponseWriter, ctx: &Ctx) {
-    let buf = match req.body {
-        Some(ref b) => &b.data[..],
-        None => panic!("no body found"),
-    };
-    let form = forms::parse_form(&buf).unwrap();
-    let search = form.get(&"q".to_string()).unwrap().clone();
-    if search[0] == b'!' {
-        let (bang, search) = split(&search);
-        let url = match ctx.get(&bang) {
+    let search = req.form_value("q".to_string()).unwrap();
+    if search.starts_with('!') {
+        let terms: Vec<&str> = search.splitn(1, ' ').collect();
+        let (bang, search): (&str, &str) = (terms[0], terms[1]);
+        let url = match ctx.get(bang) {
             Some(u) => u,
             None => ctx.get("default").unwrap(),
         };
-        let url = url.clone() + &search;
+        let url = url.clone() + search;
         res.redirect(Status::Found, url.as_bytes(), b"You are being redirected")
             .unwrap();
     } else {
-        // let search = String::from_utf8(search).unwrap();
         let url = ctx.get("default").unwrap();
-        // let url = ctx.get("default").unwrap().clone() + &search;
         res.redirect(Status::TemporaryRedirect,
                       url.as_bytes(),
                       b"You are being redirected")
             .unwrap();
     }
-}
-
-// (bang, search_term)
-fn split(search: &[u8]) -> (String, String) {
-    let mut i = 0usize;
-    for &b in search {
-        match b {
-            b' ' => {
-                // let bang = &search[1..i];
-                let bang = String::from_utf8(Vec::from(&search[1..i])).unwrap();
-                let search = String::from_utf8(Vec::from(&search[i + 1..])).unwrap();
-                return (bang, search);
-            }
-            _ => {}
-        }
-        i += 1;
-    }
-    panic!("Unable to split search");
 }
